66db88170bfed2f3befb0a204a80449a4678eda5
Lukas Bach <lukas.sebastian.bach@gmail.com>
2026-04-08 22:57:01 +0200

    Initial commit

---
+++ lua/custom/core/options.lua
@@ -0,0 +1,19 @@
+-- Display relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set the number of spaces the editor uses to display tabs 
+vim.opt.tabstop = 4
+
+-- Set the number of spaces to use for (auto)indent
+vim.opt.shiftwidth = 4
+
+-- Set the number of spaces to use when a tab is inserted
+vim.opt.softtabstop = 4
+
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+
+-- Enable highlighting of the current line
+vim.opt.cursorline = true
+
---
+++ lua/custom/init.lua
@@ -0,0 +1,9 @@
+-- Set all global variables
+require('custom.core.global')
+
+-- Bootstrap lazy.nvim
+require('custom.core.lazy')
+
+-- All vim.opt settings
+require('custom.core.options')
+
